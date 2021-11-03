633bc077e39ecf561e54da3a7fc146307fee5636
Saintrick-FM <francymalonga8@gmail.com>
2021-11-03 01:16:46 +0000

    deleted unused images, added javascript sroling and renaming id attributes for passing  the freecodecamp test

---
+++ main.js
@@ -0,0 +1,14 @@
+document.getElementById('scroll_to_title').onclick(scroll_to_title())
+document.getElementById('scroll_to_timeline').onclick(scroll_to_timeline())
+
+function scroll_to_title() {
+    document.querySelector('#tribute-info').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+function scroll_to_timeline() {
+    document.querySelector('#timeline').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
