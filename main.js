077a79af05c8dbfbec1a9476929dbf3703d9688d
Saintrick-FM <francymalonga8@gmail.com>
2021-11-03 18:32:27 +0000

    main image background-color removed, added media queries rules and some timeline informations

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-document.getElementById('scroll_to_title').onclick(scroll_to_title())
-document.getElementById('scroll_to_timeline').onclick(scroll_to_timeline())
+// document.getElementById('scroll_to_title').onclick(scroll_to_title())
+// document.getElementById('scroll_to_timeline').onclick(scroll_to_timeline())
 
-function scroll_to_title() {
-    document.querySelector('#tribute-info').scrollIntoView({
-        behavior: 'smooth'
-    });
-}
+// function scroll_to_title() {
+//     document.querySelector('#tribute-info').scrollIntoView({
+//         behavior: 'smooth'
+//     });
+// }
 
-function scroll_to_timeline() {
-    document.querySelector('#timeline').scrollIntoView({
-        behavior: 'smooth'
-    });
-}
+// function scroll_to_timeline() {
+//     document.querySelector('#timeline').scrollIntoView({
+//         behavior: 'smooth'
+//     });
+// }
+const scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 800
+})
